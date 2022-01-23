0b1f93b28a4f114a9731b073990c735593f7120e
kranold <rolfkranold@gmail.com>
2022-01-23 21:49:55 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,12 @@
+/* TO-DO LIST FOR SPACE MESSAGES
+- Create arrays with data
+    - 1:
+    - 2:
+    - 3: 
+- Create random selector function 
+- Create combining function
+- Combine into output
+
+*/ 
+
+console.log("Rolfs first program")
